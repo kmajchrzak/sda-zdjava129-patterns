ed9cb15b83cd8eee4b086b391d933a76fcbfcd84
Karolina Majchrzak <karolina.majchrzak966@gmail.com>
2022-11-20 13:32:23 +0100

    StudentBuilder

--- src/main/java/pl/sdacademy/java/patterns/Student.java
+++ src/main/java/pl/sdacademy/java/patterns/Student.java
@@ -1,9 +1,14 @@
 package pl.sdacademy.java.patterns;
 
-class Student implements HasPerson{
+class Student implements HasPerson {
 
     private Person person;
     private String groupCode;
+
+    public Student(Person person, String groupCode) {
+        this.person = person;
+        this.groupCode = groupCode;
+    }
 
     @Override
     public Person getPerson() {
